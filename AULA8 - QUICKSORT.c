4be78fe9e981b8e0c80a34d472375f01b2bfb57b
gabrielvvs1 <87244095+gabrielvvs1@users.noreply.github.com>
2024-05-21 14:06:35 -0300

    AULA8 - QUICKSORT.c

--- AULA8 - QUICKSORT.c
+++ AULA8 - QUICKSORT.c
@@ -2,56 +2,62 @@
 #include <stdlib.h>
 #include <time.h>
 
-//algoritmo Quicksort
-
 #define VERDADEIRO 1
 #define FALSO 0
 
-void swap(int *A, int *B); // para trocar 2 elementos de lugar
+void swap(int *A, int *B); // Trocar dois elementos de lugar
 int particao(int V[], int inf, int sup);
 void quicksort(int V[], int inf, int sup);
+int comparar(int *A, int *B);
 
-int main (){
+int main() {
+
     system ("cls");
-    int V[] = {3, 6, 4, 5, 1, 7, 2};
-    int N = sizeof(V)/sizeof(int);
-    quicksort(V, 0 , N -1);
-    for(int i=0; i<N; i++) printf("%d",V[i]);
-    printf("\n");
+//    int V[] = {3, 6, 4, 5, 1, 7, 2};
+        int V[] = {25, 17, 31, 13, 2, 19, 8, 14, 7, 1, 22, 10, 3, 12, 9, 5, 16, 11, 4};
 
+    int N = sizeof(V)/sizeof(int);
+    printf("Tamanho: %d\n", N);
+    quicksort(V, 0, N - 1);
+    for(int i = 0; i < N; i++) printf("%d ", V[i]);
+    printf("\n");
     return 0;
 }
 
-
 void swap(int *A, int *B){
-   // printf("%d %d %d %d\n", A, B, *A, *B);
+    //printf("%d %d %d %d \n", A, B, *A, *B);
     int temp = *A;
     *A = *B;
     *B = temp;
-
 }
 
 int particao(int V[], int inf, int sup){
     int Pivot = V[(inf + sup) / 2];
-    int i = inf; // i: indice para percorrer subvetor da esquerda.
-    int j =sup; // j: indice para percorrer subvetor da direita.
+    int i = inf; // i: índice para percorrer subvetor da esquerda
+    int j = sup; // j: índice para percorrer subvetor da direita
     while(i <= j) {
-        while (V[i] < Pivot) i++;
-        while (V[j] > Pivot) j--;
-        if(i <= j){
+        while (comparar(&V[i], &Pivot) < 0) i++;
+        while (comparar(&V[j], &Pivot) > 0) j--;
+        if(i <= j) {
             swap(&V[i], &V[j]);
             i++;
             j--;
         }
     }
-    return (i); // i é a posicao do pivô
+    return (i); // i é a posição do pivô.
 }
 
 void quicksort(int V[], int inf, int sup){
-    if(inf < sup){
+    if (inf < sup) {
         int P = particao(V, inf, sup);
+        printf("%d %d\n", P, V[P]);
         quicksort(V, inf, P - 1);
-        quicksort(V, P + 1, sup);
-
+        quicksort(V, P , sup);
     }
+}
+
+int comparar(int *A, int *B) {
+
+    return(*A - *B);
+
 }
