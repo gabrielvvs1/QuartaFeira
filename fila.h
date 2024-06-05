b9e2e31e7b71a0da668a4e12bb36307e3dbe2415
gabrielvvs1 <87244095+gabrielvvs1@users.noreply.github.com>
2024-06-05 08:57:18 -0300

    fila.h

--- fila.h
+++ fila.h
@@ -1,21 +1,22 @@
 #ifndef FILA_H
 #define FILA_H
 
-typedef struct Item{ // estrutara do i  tem da fila
+typedef struct Item {
     int Chave;
-    struct Item* Proximo;
-}Item;
+    struct Item * Proximo;
+} Item;
 
-typedef struct Fila{ // estrutura da fila
+typedef struct Fila {
     Item * Inicio;
     Item * Fim;
     int Tamanho;
-}Fila;
+} Fila;
 
 Fila * criarFila();
 
 void enfileirar(Fila *F, Item * Elemento);
+void mostrarFila(Fila *F);
 
-
+Item *desenfileirar(Fila *F);
 
 #endif
